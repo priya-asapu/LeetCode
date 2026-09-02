8678f1ba506c81da9e52b785b54633efdc16b1c1
priya-asapu <asapupriya6@gmail.com>
2026-09-02 14:25:18 +0530

    Time: 23 ms (57.78%) | Memory: 19.4 MB (55.04%) - LeetSync

---
+++ 4280-count-digit-appearances/count-digit-appearances.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countDigitOccurrences(self, nums: list[int], digit: int) -> int:
+        cnt=0
+        for num in nums:
+            cnt+=str(num).count(str(digit))
+        return cnt
