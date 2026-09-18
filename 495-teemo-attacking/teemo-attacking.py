ea6c5b11b0a22d714fa7219396483f18fda45335
priya-asapu <asapupriya6@gmail.com>
2026-09-18 20:50:16 +0530

    Time: 11 ms (82.34%) | Memory: 20.6 MB (92.38%) - LeetSync

---
+++ 495-teemo-attacking/teemo-attacking.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries: list[int], duration: int) -> int:
+        total = 0
+        for i in range(len(timeSeries) - 1):
+            gap = timeSeries[i + 1] - timeSeries[i]
+            if gap < duration:
+                total += gap
+            else:
+                total += duration
+        if len(timeSeries) > 0:
+            total += duration
+        return total
